e8172491e8d865a8f0cc0b1ca3a330ed7cc5fd41
Dunked0n <danielvontrotha@outlook.com>
2020-09-06 11:03:31 -0400

    trying to make a setup script

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
-print("Please enter the directory you wish to back up your files to:")
-dir = input()
+import os
 
+gendir = 'mkdir'
+
+print("Please enter the directory you wish to back up your server instance to:")
+dir = input("/")
+
+os.system(gendir dir)
